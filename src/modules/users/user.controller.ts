9657b549db575e362951279ac29b7d64624a8dfa
Anastasiya Penya <53582712+CurlyAnastasi@users.noreply.github.com>
2021-06-02 21:03:33 +0300

    12.2_user_add_route

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,9 +1,18 @@
+import { productsService } from './../products/product.service';
+import { INewProduct } from './../../common/dtos/new.product.dto';
+import { ValidatedRequest } from 'express-joi-validation';
 import { Request, Response } from 'express';
 import { json } from 'sequelize';
 import { asyncHandler } from '../../common/helpers/async.handler';
 import { BaseView } from '../../common/views/view';
 import { userService } from './user.service';
 
-class UserController {}
+class UserController {
+    addProduct = asyncHandler(async (req: ValidatedRequest<INewProduct>, res: Response): Promise<void> => {
+		const { product } = req.body;
+		const newProduct = await productsService.addNewProduct(product);
+		BaseView.buildSuccessView(res, newProduct);
+	});
+}
 
 export const userController = new UserController();
--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -1,9 +1,12 @@
+import { salesmanMiddleware } from './../../common/middleware/salesman.middleware';
 import express from 'express';
 import { authenticate } from '../../common/middleware/auth.middleware';
 import { userController } from './user.controller';
+import { newProductDto, validator } from '../../common/dtos/new.product.dto';
 
 const userRoute = express.Router();
 
 userRoute.get('/', authenticate);
+userRoute.post('/add', authenticate, salesmanMiddleware,  validator.body(newProductDto), userController.addProduct);
 
 export { userRoute };
