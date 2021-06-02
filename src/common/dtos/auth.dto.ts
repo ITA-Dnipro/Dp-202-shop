a58e0efa3974e9f89cde51409433693a14702c9c
Anastasiya Penya <53582712+CurlyAnastasi@users.noreply.github.com>
2021-06-02 14:14:08 +0300

    spaces were added

--- src/common/dtos/auth.dto.ts
+++ src/common/dtos/auth.dto.ts
@@ -1,5 +1,5 @@
 import * as Joi from 'joi';
-import {createValidator} from 'express-joi-validation';
+import { createValidator } from 'express-joi-validation';
 
 export const validator = createValidator();
 
