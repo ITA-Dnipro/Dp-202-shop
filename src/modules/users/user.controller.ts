ad549b440a5177eb400b45a7200de9eb5076f8f8
Katerina <68004503+katerinassh@users.noreply.github.com>
2021-06-02 14:18:09 +0300

    merged conflict resolved

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -4,14 +4,6 @@ import { asyncHandler } from '../../common/helpers/async.handler';
 import { BaseView } from '../../common/views/view';
 import { userService } from './user.service';
 
-<<<<<<< HEAD
 class UserController {}
-=======
-class UserController {
-	// success = asyncHandler(async (req: Request, res: Response): Promise<any> => {
-	// 	return res.json({ message: 'Success' });
-	// });
-}
->>>>>>> fb43d303b47bba1f2ebb8f296d6aa3168fc83064
 
 export const userController = new UserController();
