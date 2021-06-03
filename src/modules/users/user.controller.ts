b42ef88f158493030c8592124f73e6a2369273d3
ShumerPlema <40871531+ShumerPlema@users.noreply.github.com>
2021-06-03 15:19:58 +0300

    13 edit products salesman (#79)
    * feat(user): add editSalesmanProduct method; add getSalesmanProductById method; refactor idIsExist method
    
    * Update product.service.ts
    
    fix(product.service): change object key
    
    * refactor(user,service): rename variable

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,18 +1,45 @@
+import { NextFunction, Request, Response } from 'express';
 import { productsService } from './../products/product.service';
-import { INewProduct } from './../../common/dtos/new.product.dto';
-import { ValidatedRequest } from 'express-joi-validation';
-import { Request, Response } from 'express';
 import { json } from 'sequelize';
+import { ValidatedRequest } from 'express-joi-validation';
 import { asyncHandler } from '../../common/helpers/async.handler';
 import { BaseView } from '../../common/views/view';
 import { userService } from './user.service';
+import { INewProduct, IProduct } from '../../common/dtos/new.product.dto';
+import { ProductAttributes } from '../../db/models/Product.model';
 
 class UserController {
-    addProduct = asyncHandler(async (req: ValidatedRequest<INewProduct>, res: Response): Promise<void> => {
+	public getSalesmanProductById = asyncHandler(
+		async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+			const salesmanId: number = res.locals.user.id;
+			const { id } = req.params;
+			const product: IProduct = await userService.getSalesmanProductById(
+				salesmanId,
+				+id,
+			);
+			BaseView.buildSuccessView(res, product);
+		},
+	);
+
+	public editSalesmanProduct = asyncHandler(
+		async (
+			req: ValidatedRequest<INewProduct>,
+			res: Response,
+			next: NextFunction,
+		): Promise<void> => {
+			const salesmanId: number = res.locals.user.id;
+			const { id } = req.params;
+			const { product } = req.body;
+			const updatedProduct: Array<ProductAttributes> | void =
+				await userService.editSalesmanProduct(salesmanId, +id, product);
+			BaseView.buildSuccessView(res, updatedProduct);
+		},
+	);
+
+ public addProduct = asyncHandler(async (req: ValidatedRequest<INewProduct>, res: Response): Promise<void> => {
 		const { product } = req.body;
 		const newProduct = await productsService.addNewProduct(product);
 		BaseView.buildSuccessView(res, newProduct);
 	});
 }
-
 export const userController = new UserController();
--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -2,11 +2,35 @@ import { salesmanMiddleware } from './../../common/middleware/salesman.middlewar
 import express from 'express';
 import { authenticate } from '../../common/middleware/auth.middleware';
 import { userController } from './user.controller';
+import { salesmanMiddleware } from '../../common/middleware/salesman.middleware';
+import { idDto } from '../../common/dtos/id.dto';
+import { validator } from '../../common/dtos/search.params.dto';
 import { newProductDto, validator } from '../../common/dtos/new.product.dto';
 
 const userRoute = express.Router();
 
 userRoute.get('/', authenticate);
-userRoute.post('/add', authenticate, salesmanMiddleware,  validator.body(newProductDto), userController.addProduct);
+userRoute.get(
+	'/edit/:id',
+	authenticate,
+	salesmanMiddleware,
+	validator.params(idDto),
+	userController.getSalesmanProductById,
+);
+userRoute.put(
+	'/edit/:id',
+	authenticate,
+	salesmanMiddleware,
+	validator.params(idDto),
+	validator.body(newProductDto),
+	userController.editSalesmanProduct,
+);
+userRoute.post(
+  '/add', 
+  authenticate, 
+  salesmanMiddleware,  
+  validator.body(newProductDto), 
+  userController.addProduct
+);
 
 export { userRoute };
