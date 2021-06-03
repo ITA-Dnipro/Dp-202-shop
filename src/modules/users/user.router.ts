032cbd5ea93b4ca99d63ba478f922bf68ee37571
Egor <marderosov7@gmail.com>
2021-06-03 15:28:40 +0300

    fix(user.router) delete simillar lines

--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -1,10 +1,8 @@
-import { salesmanMiddleware } from './../../common/middleware/salesman.middleware';
 import express from 'express';
 import { authenticate } from '../../common/middleware/auth.middleware';
 import { userController } from './user.controller';
 import { salesmanMiddleware } from '../../common/middleware/salesman.middleware';
 import { idDto } from '../../common/dtos/id.dto';
-import { validator } from '../../common/dtos/search.params.dto';
 import { newProductDto, validator } from '../../common/dtos/new.product.dto';
 
 const userRoute = express.Router();
@@ -26,11 +24,11 @@ userRoute.put(
 	userController.editSalesmanProduct,
 );
 userRoute.post(
-  '/add', 
-  authenticate, 
-  salesmanMiddleware,  
-  validator.body(newProductDto), 
-  userController.addProduct
+	'/add',
+	authenticate,
+	salesmanMiddleware,
+	validator.body(newProductDto),
+	userController.addProduct,
 );
 
 export { userRoute };
